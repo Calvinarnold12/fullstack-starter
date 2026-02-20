71dab0f8797d574cd9957651a33ca3bb5ec4dbbd
Calvin Arnold <143022322+Calvinarnold12@users.noreply.github.com>
2026-02-20 16:21:41 -0700

    Update urls.py

--- django-helloworld/helloworld/urls.py
+++ django-helloworld/helloworld/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path('admin/', admin.site.name),
-    path('', views.hello_world, name='index'),    # This handles the main page
-    path('hello/', views.hello_world, name='hello_world'), # Add this line for the button!
-]
+    path('admin/', admin.site.urls),
+    path('', views.hello_world, name='index'),               
+    path('hello/', views.hello_world, name='hello_world'),    # 
