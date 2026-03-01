6d1df1a6fa32d738a02b0515286de0f59fe6b557
Zachary Johnston <ZacharyJohnston324@u.boisestate.edu>
2026-02-28 20:39:57 -0700

    Venv working-ish

--- docs/deploy-node/install-packages.sh
+++ docs/deploy-node/install-packages.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # This script installs the necessary dependencies on an EC2 instance.
 sudo apt-get update
+#Install venv
+sudo apt install python3.12-venv
 # Start a python virtual environment (.venv)
 # Note: Linux based systems use python heavily and a virtual environment protects the system's packages
-sudo python -m venv app-env
+sudo python3 -m venv app-env
+sudo echo "$(ls -l)"
 # Opens and activates virtual environment (venv)
-sudo source app-env/bin/activate
+sudo bash source app-env/bin/activate
 # Pip package installer (Should be in the venv)
-sudo python -m ensurepip --upgrade
+sudo python3 -m ensurepip --upgrade
 # Install Django
-sudo python -m pip install Django
+sudo python3 -m pip install Django
 
 # Apparently this method shouldn't need a refresh
 echo "Installation script completed."
