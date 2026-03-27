ccfa14706f4fd8050dbf931b87b086994e246c5a
Calvin Arnold <calvinarnold@u.boisestate.edu>
2026-03-27 10:54:40 -0600

    I think this may be the issue, This folder is routing.

--- django-helloworld/helloworld/urls.py
+++ django-helloworld/helloworld/urls.py
@@ -4,6 +4,8 @@ from hello import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello_world, name='index'),
-    path('hello/', views.hello_world, name='hello_world'),
+    path('', views.index_view, name='index'),                     # http://35.90.198.162/
+    path('hello/', views.hello_world, name='hello'),              # http://35.90.198.162/hello/
+    path('hello/register/', views.register_view, name='register'),# http://35.90.198.162/hello/register/
+    path('hello/login/', views.login_view, name='login'),         # http://35.90.198.162/hello/login/
 ]
