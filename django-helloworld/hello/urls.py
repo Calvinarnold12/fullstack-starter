1cda0cdd2cf264d408f15a418d85a32c51205f08
Calvin Arnold <calvinarnold@u.boisestate.edu>
2026-03-27 10:42:11 -0600

    Implement user registration linked to database

--- django-helloworld/hello/urls.py
+++ django-helloworld/hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
-    path('hello/', views.hello_world, name='hello_world_link'),
+    path('', views.hello_world, name='hello'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
 ]
--- django-helloworld/hello/views.py
+++ django-helloworld/hello/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import PlayerProfile
 
 def hello_world(request):
-    """
-    View function that renders the Hello World page.
-    Demonstrates a simple request/response cycle that can be debugged.
-    """
-    # import pdb; pdb.set_trace()  Debugger breakpoint - execution pauses here
+    return render(request, "hello/hello_world.html")
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Create the linked PlayerProfile for our stats
+            PlayerProfile.objects.create(user=user)
+            login(request, user)
+            return redirect('hello') # Send them to the main page
+    else:
+        form = UserCreationForm()
     
-    context = {
-        'message': 'Hello World!',
-        'title': 'Hello World App',
-    }
-    return render(request, 'hello/index.html', context)
+    return render(request, "hello/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('hello')
+    else:
+        form = AuthenticationForm()
+        
+    return render(request, "hello/login.html", {"form": form})
