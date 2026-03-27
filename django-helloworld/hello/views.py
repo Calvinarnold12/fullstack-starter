c9427954497e235cfaf2c092d6f51d6f68196f38
Calvin Arnold <calvinarnold@u.boisestate.edu>
2026-03-27 10:53:23 -0600

    Fiddlesticks

--- django-helloworld/hello/views.py
+++ django-helloworld/hello/views.py
@@ -3,24 +3,29 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import PlayerProfile
 
+# --- NEW: Serve the root index.html ---
+def index_view(request):
+    return render(request, "hello/index.html", {"message": "Welcome to our CS 408 Project!"})
+
+# --- Existing Views ---
 def hello_world(request):
     return render(request, "hello/hello_world.html")
 
 def register_view(request):
+    # ... (Keep your existing register code here)
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # Create the linked PlayerProfile for our stats
             PlayerProfile.objects.create(user=user)
             login(request, user)
-            return redirect('hello') # Send them to the main page
+            return redirect('hello')
     else:
         form = UserCreationForm()
-    
     return render(request, "hello/register.html", {"form": form})
 
 def login_view(request):
+    # ... (Keep your existing login code here)
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -29,5 +34,4 @@ def login_view(request):
             return redirect('hello')
     else:
         form = AuthenticationForm()
-        
     return render(request, "hello/login.html", {"form": form})
