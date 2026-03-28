d41154623fd76c481bb21788074c85379123fdf1
Zach Johnston <157074462+ZachCJ@users.noreply.github.com>
2026-03-28 16:09:50 -0600

    Register and login styling (#20)
    * Login box created with styled forms
    
    * Adding branch to CI (will be removed before merge)
    
    * added temp logo to login
    
    * login Styling Done
    
    * Login and Registration Styled
    
    * Prep branch for merge

--- django-helloworld/hello/views.py
+++ django-helloworld/hello/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import PlayerProfile
+from .forms import UserLoginForm
+from .forms import UserRegistrationForm
 
 # --- NEW: Serve the root index.html ---
 def index_view(request):
@@ -14,24 +16,24 @@ def hello_world(request):
 def register_view(request):
     # ... (Keep your existing register code here)
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             PlayerProfile.objects.create(user=user)
             login(request, user)
             return redirect('hello')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, "hello/register.html", {"form": form})
 
 def login_view(request):
     # ... (Keep your existing login code here)
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+        form = UserLoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('hello')
     else:
-        form = AuthenticationForm()
+        form = UserLoginForm()
     return render(request, "hello/login.html", {"form": form})
