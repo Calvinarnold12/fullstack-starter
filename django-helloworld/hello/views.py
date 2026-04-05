b731caec2c46309a36e15ab983d723f06024429a
Calvin Arnold <calvinarnold@u.boisestate.edu>
2026-04-05 13:15:40 -0600

    Fix Logout bug

--- django-helloworld/hello/views.py
+++ django-helloworld/hello/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import PlayerProfile
 from .forms import UserLoginForm
@@ -39,4 +39,4 @@ def login_view(request):
     return render(request, "hello/login.html", {"form": form})
 def logout_view(request):
     logout(request) # This destroys the session
-    return redirect('home')
+    return redirect('hello')
--- django-helloworld/helloworld/urls.py
+++ django-helloworld/helloworld/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('hello/', views.hello_world, name='hello'),              # http://35.90.198.162/hello/
     path('hello/register/', views.register_view, name='register'),# http://35.90.198.162/hello/register/
     path('hello/login/', views.login_view, name='login'),         # http://35.90.198.162/hello/login/
+    path('hello/logout/', views.logout_view, name='logout'),      # http://35.90.198.162/hello/logout/
 ]
