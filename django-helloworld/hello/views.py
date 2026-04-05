cdb9b6c6d3915fcc3688ff7219abdca0770add0f
Calvin Arnold <calvinarnold@u.boisestate.edu>
2026-04-05 12:38:32 -0600

    Added logout view

--- django-helloworld/hello/views.py
+++ django-helloworld/hello/views.py
@@ -35,3 +35,6 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "hello/login.html", {"form": form})
+def logout_view(request):
+    logout(request) # This destroys the session
+    return redirect('home')
